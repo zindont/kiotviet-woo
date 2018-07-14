a9747f1c9f7e831c528cb77728a3fef5d728acd8
Ân <info@zindo.info>
2018-07-14 14:30:41 +0700

    Include CMB2 2.4.2

--- includes/class-kiotviet-woo.php
+++ includes/class-kiotviet-woo.php
@@ -97,7 +97,16 @@ class Kiotviet_Woo {
 	 * @access   private
 	 */
 	private function load_dependencies() {
-
+		/**
+		 * CMB2
+		 * Get the bootstrap! If using the plugin from wordpress.org, REMOVE THIS!
+		 */
+		if ( file_exists( dirname( __FILE__ ) . '/cmb2/init.php' ) ) {
+			require_once dirname( __FILE__ ) . '/cmb2/init.php';
+		} elseif ( file_exists( dirname( __FILE__ ) . '/CMB2/init.php' ) ) {
+			require_once dirname( __FILE__ ) . '/CMB2/init.php';
+		}
+		
 		/**
 		 * The class responsible for orchestrating the actions and filters of the
 		 * core plugin.
