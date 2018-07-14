0dd45db33fc3dbb82c20f5bd9c32b40493ef4e88
Ân <info@zindo.info>
2018-07-14 13:52:23 +0700

    Remove public facing files

--- includes/class-kiotviet-woo.php
+++ includes/class-kiotviet-woo.php
@@ -77,7 +77,6 @@ class Kiotviet_Woo {
 		$this->load_dependencies();
 		$this->set_locale();
 		$this->define_admin_hooks();
-		$this->define_public_hooks();
 
 	}
 
@@ -116,12 +115,6 @@ class Kiotviet_Woo {
 		 */
 		require_once plugin_dir_path( dirname( __FILE__ ) ) . 'admin/class-kiotviet-woo-admin.php';
 
-		/**
-		 * The class responsible for defining all actions that occur in the public-facing
-		 * side of the site.
-		 */
-		require_once plugin_dir_path( dirname( __FILE__ ) ) . 'public/class-kiotviet-woo-public.php';
-
 		$this->loader = new Kiotviet_Woo_Loader();
 
 	}
@@ -156,22 +149,6 @@ class Kiotviet_Woo {
 
 		$this->loader->add_action( 'admin_enqueue_scripts', $plugin_admin, 'enqueue_styles' );
 		$this->loader->add_action( 'admin_enqueue_scripts', $plugin_admin, 'enqueue_scripts' );
-
-	}
-
-	/**
-	 * Register all of the hooks related to the public-facing functionality
-	 * of the plugin.
-	 *
-	 * @since    1.0.0
-	 * @access   private
-	 */
-	private function define_public_hooks() {
-
-		$plugin_public = new Kiotviet_Woo_Public( $this->get_plugin_name(), $this->get_version() );
-
-		$this->loader->add_action( 'wp_enqueue_scripts', $plugin_public, 'enqueue_styles' );
-		$this->loader->add_action( 'wp_enqueue_scripts', $plugin_public, 'enqueue_scripts' );
 
 	}
 
