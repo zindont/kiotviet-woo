fac552dfbeca0482a70c3e8fb7f84cd631a36917
Ân <info@zindo.info>
2018-07-14 10:45:57 +0700

    Update version & description

--- includes/class-kiotviet-woo.php
+++ includes/class-kiotviet-woo.php
@@ -67,8 +67,8 @@ class Kiotviet_Woo {
 	 * @since    1.0.0
 	 */
 	public function __construct() {
-		if ( defined( 'PLUGIN_NAME_VERSION' ) ) {
-			$this->version = PLUGIN_NAME_VERSION;
+		if ( defined( 'KIOTVIET_WOO_VERSION' ) ) {
+			$this->version = KIOTVIET_WOO_VERSION;
 		} else {
 			$this->version = '1.0.0';
 		}
--- kiotviet-woo.php
+++ kiotviet-woo.php
@@ -15,7 +15,7 @@
  * @wordpress-plugin
  * Plugin Name:       KiotViet Woocommerce
  * Plugin URI:        https://zindo.info
- * Description:       This is a short description of what the plugin does. It's displayed in the WordPress admin area.
+ * Description:       Plugin đồng bộ KiotViet với Woocommerce
  * Version:           1.0.0
  * Author:            Linh Ân
  * Author URI:        https://zindo.info
@@ -35,7 +35,7 @@ if ( ! defined( 'WPINC' ) ) {
  * Start at version 1.0.0 and use SemVer - https://semver.org
  * Rename this for your plugin and update it as you release new versions.
  */
-define( 'PLUGIN_NAME_VERSION', '1.0.0' );
+define( 'KIOTVIET_WOO_VERSION', '1.0.0' );
 
 /**
  * The code that runs during plugin activation.
