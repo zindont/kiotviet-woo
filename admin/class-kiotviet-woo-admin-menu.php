75dac3e7f64ebff61d32529f0c5455d6f611c005
Ân <info@zindo.info>
2018-07-16 22:43:19 +0700

    Add admin settings page

--- admin/class-kiotviet-woo-admin-menu.php
+++ admin/class-kiotviet-woo-admin-menu.php
@@ -36,10 +36,25 @@ class Kiotviet_Woo_Admin_Menu extends Kiotviet_Woo_Admin {
 	        __( 'KiotViet Woo', 'kiotviet-woo' ),
 	        'manage_options',
 	        'kiotviet-woo',
-	        '',
+	        null,
 	        'dashicons-store',
 	        56.5
 	    );
+
+	    $settings_page = add_submenu_page( 'kiotviet-woo',
+	    	__( 'Cài đặt', 'kiotviet-woo' ),
+	    	__( 'Cài đặt', 'kiotviet-woo' ),
+	    	'manage_options', 
+	    	'kiotviet-woo-settings',
+	    	array( $this, 'kiotviet_woo_admin_settings_page' )
+	    );
 	}
+
+	/**
+	 * Init the settings page.
+	 */
+	public function kiotviet_woo_admin_settings_page() {
+		echo "<h1>OUTPUT SETTINGPAGE</h1>";
+	}	
 
 }
